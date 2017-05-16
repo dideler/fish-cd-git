b66559565b133625925eabbf113843b25ae6a2d1
Dennis Ideler <ideler.dennis@gmail.com>
2017-05-16 22:54:39 +0100

    Be more forgiving :angel:
    Now handles `gh user/repo` style input.
    
    Not documenting this feature since I don't want to officially support
    it or encourage its use, as that means handling edge cases.
    
    The intention is to help users who copy-paste the user/repo path from
    GitHub and don't want to replace "/" with a " ".

--- gh.fish
+++ gh.fish
@@ -17,6 +17,10 @@ function gh -d "Navigate to cloned repos from github.com" -a user repo
   if contains -- -h $argv; or contains -- --help $argv
     __gh_print_usage
     return 0
+  else if test $argc -eq 1  # Attempt to autocorrect "user/repo" input.
+    set --local user_repo (__gh_parse_args $user)
+    set user $user_repo[1]
+    set repo $user_repo[2]
   else if test $argc -ne 2
     printf "Error: %s expected 2 arguments, got %d\n\n" $_ $argc
     __gh_print_usage
@@ -35,4 +39,8 @@ end
 
 function __gh_clone_repo_if_missing --argument path git_host user repo
   test -d $path; or command git clone -q git@$git_host:$user/$repo.git $path
+end
+
+function __gh_parse_args --argument user_repo
+  string split --max=1 / (string trim --char=/ $user_repo)
 end
