c42d1a37e1b1578cd99d79733151ca4eb25083f7
Dennis Ideler <ideler.dennis@gmail.com>
2019-01-06 18:35:43 +0000

    v1.1.3
    Update build to work with fish v3 and fisher v3.
    
    No user facing changes.

--- gh.fish
+++ gh.fish
@@ -3,7 +3,7 @@ function __gh_setup
   or set --universal GH_BASE_DIR $HOME
 
   set --global gh_cmd_name (basename (status --current-filename) .fish)
-  set --global gh_version "1.1.2"
+  set --global gh_version "1.1.3"
   set --global gh_git_host github.com
 end
 
